ba9df1c2fa8d19adac925cc2b23cfe9b250d2316
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-03 14:45:19 -0500

    add the path for user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { connect } = require('./src/db');
+const usersRouter = require('./src/routes/user');
 
 const port = process.env.PORT || 8080;
 
@@ -10,6 +11,8 @@ connect();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`)
