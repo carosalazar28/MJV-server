1d17dec6902822fd879a586769fd8c67d44d2c4e
Carolina <cindy.s.r.28@hotmail.com>
2022-03-21 16:12:25 -0500

    add field for birthday

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,32 +1,38 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema ({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+    },
+    birthdayDate: {
+      type: Date,
+    },
+    rolDeveloper: {
+      type: String,
+    },
+    job: {
+      type: Boolean,
+      default: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-  },
-  rolDeveloper: {
-    type: String,
-  },
-  job: {
-    type: Boolean,
-    default: true,
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model('User', userSchema);
 
